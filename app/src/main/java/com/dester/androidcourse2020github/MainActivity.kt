391eb13772a0066c1984b2cc8c65ee37b9905090
Destered <timba1721@mail.ru>
2020-09-09 17:54:17 +0300

    hw1-complete

--- app/src/main/java/com/dester/androidcourse2020github/MainActivity.kt
+++ app/src/main/java/com/dester/androidcourse2020github/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cr1 = Creature("Bird","Animal")
+        cr1.test()
+        val p1 = Pokemon("Slowpoke","Pokemon",12)
+        p1.test()
+        val p2 = Pokemon("Pickachu","Pokemon",22)
+        p2.test()
+        println(p1.compareTo(p2))
     }
 }
