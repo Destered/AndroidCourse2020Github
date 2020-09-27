f99214d316b199f19d739e2eacb38680dc1bcc4b
Destered <the_bagin@mail.ru>
2020-09-27 23:32:59 +0300

    hw3-complete

--- app/src/main/java/com/dester/androidcourse2020github/MainActivity.kt
+++ app/src/main/java/com/dester/androidcourse2020github/MainActivity.kt
@@ -32,17 +32,35 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun endEdit(){
-        tv_name.apply {
-            this.text = et_name.text
-            visibility = View.VISIBLE
+    private fun endEdit() : Boolean{
+        if(et_name.text.trim().isNotEmpty()) {
+            tv_name.apply {
+                this.text = et_name.text.trim()
+                visibility = View.VISIBLE
+            }
+            et_name.visibility = View.INVISIBLE
+            return true
+        } else {
+            et_name.setText("Введите имя")
+            return false
         }
-        et_name.visibility = View.INVISIBLE
     }
 
 
-    @SuppressLint("ResourceType")
     fun init(){
+        initImage()
+        initExtras()
+        et_name.visibility = View.INVISIBLE
+        et_name.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                return@OnEditorActionListener endEdit()
+            }
+            false
+        })
+    }
+
+    @SuppressLint("ResourceType")
+    fun initImage(){
         imageView_avatar.setImageResource(R.raw.avatar)
         imageView_photo1.setImageResource(R.raw.photo1)
         imageView_photo2.setImageResource(R.raw.photo2)
@@ -56,15 +74,13 @@ class MainActivity : AppCompatActivity() {
         imageView_addIcon2.setImageResource(R.raw.addicon2)
         imageView_addIcon3.setImageResource(R.raw.addicon3)
         imageView_addIcon4.setImageResource(R.raw.addicon4)
-        et_name.visibility = View.INVISIBLE
-        et_name.setOnEditorActionListener(OnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                endEdit()
-                return@OnEditorActionListener true
-            }
-            false
-        })
+    }
 
+    @SuppressLint("SetTextI18n")
+    fun initExtras(){
+        val school = intent.getStringExtra("school") ?: "Вы не должны видеть этого..."
+        tv_school.text = "Место учёбы: $school"
+        tv_name.text = intent.getStringExtra("fullname") ?: "Вы не должны видеть этого..."
     }
 
 }
