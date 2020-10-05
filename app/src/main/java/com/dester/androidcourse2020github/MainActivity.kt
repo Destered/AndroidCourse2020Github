bfd73018d4e77120bac5d104adac744f56241735
Destered <the_bagin@mail.ru>
2020-10-05 14:29:41 +0300

    hw3 (complete)

--- app/src/main/java/com/dester/androidcourse2020github/MainActivity.kt
+++ app/src/main/java/com/dester/androidcourse2020github/MainActivity.kt
@@ -6,36 +6,27 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
-   /*  {Немного не так понято задание было
-   private val REQUEST_DATA_TYPE = 1
-    val DATA_KEY = "DATA"
-    val ACCESS_MESSAGE = "ACCESS_MESSAGE"
+    private val PICKFILE_RESULT_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btn_sendrequest.setOnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
-            val info = et_name.text.toString().trim()
-            if(info.isNotEmpty()) {
-                intent.putExtra(DATA_KEY, info)
-                startActivityForResult(intent, REQUEST_DATA_TYPE)
-            } else{
-                Toast.makeText(this, "Input data", Toast.LENGTH_SHORT).show()
-            }
+        btn_choosefile.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "file/*"
+            startActivityForResult(intent, PICKFILE_RESULT_CODE)
         }
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == REQUEST_DATA_TYPE){
-            if(resultCode == RESULT_OK){
-                val dataString = data?.getStringExtra(ACCESS_MESSAGE) ?: "Null"
-                tv_result.text = dataString
+        when (requestCode) {
+            PICKFILE_RESULT_CODE -> if (resultCode == RESULT_OK) {
+                val FilePath = data?.data?.path ?: "Null"
+                Toast.makeText(this,"File path:$FilePath", Toast.LENGTH_SHORT).show()
             }
-        }else {
-            super.onActivityResult(requestCode, resultCode, data)
         }
-    }}*/
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 }
--- app/src/main/java/com/dester/androidcourse2020github/SecondActivity.kt
+++ app/src/main/java/com/dester/androidcourse2020github/SecondActivity.kt
@@ -5,70 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
 
 
 class SecondActivity : AppCompatActivity() {
-/* Немного не так было понято задание
-   {val DATA_KEY = "DATA"
-    val ACCESS_MESSAGE = "ACCESS_MESSAGE"
-    lateinit var id : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val extras = intent.extras
-        if(extras != null){
-            id = extras.getString(DATA_KEY) ?: "null"
-            tv_objectname.text = id
-        }
-
-        btn_cancle.setOnClickListener {
-            setResult(RESULT_CANCELED)
-            finish()
-        }
-
-        btn_send.setOnClickListener{
-            val check = rg_radiogroup.checkedRadioButtonId
-            if(check == -1){
-                Toast.makeText(this,"Choose class",Toast.LENGTH_SHORT).show()
-            } else{
-                val answer = "Объект:$id\n" + returnClass(check) + checkSwitch()
-                sendResult(answer)
-            }
+        val intent = intent
+        val action = intent.action
+        if(action.equals(Intent.ACTION_SEND)){
+            tv_sendget.text = intent.getStringExtra(Intent.EXTRA_TEXT)
         }
     }
-
-    private fun returnClass(code: Int):String{
-        var objectClass = ""
-        when(code){
-            R.id.rb_easy-> objectClass = "Безопасный"
-            R.id.rb_medium-> objectClass = "Евклид"
-            R.id.rb_hard-> objectClass = "Кетер"
-        }
-        return "Класс объекта: $objectClass\n"
-    }
-
-    private fun checkSwitch():String{
-        var answer = ""
-        if(sw_accident.isChecked){
-            answer += "Был инцидент [УДАЛЕНО]\n"
-        } else{
-            answer += "Инцидентов не было\n"
-        }
-        if(sw_accses.isChecked){
-            answer+= "Доступен для исследований"
-        } else{
-            answer+= "Не доступен для исследований"
-        }
-        return answer
-    }
-
-    private fun sendResult(message:String){
-        val data = Intent()
-        data.putExtra(ACCESS_MESSAGE, message)
-        setResult(RESULT_OK, data)
-        finish()
-    }
-    }*/
 
 }
