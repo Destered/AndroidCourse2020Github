68c20f8b02f27d668511deea2b6bfdb04e5f029e
Timur Boltachev <timur.boltachev@joy-dev.com>
2021-02-21 18:13:39 +0300

    Погода доделал

---
+++ app/src/main/java/com/dester/androidcourse2020github/api/CityResponse.kt
@@ -0,0 +1,15 @@
+package com.dester.androidcourse2020github.api
+
+import com.dester.androidcourse2020github.WeatherResponse
+import com.google.gson.annotations.SerializedName
+
+data class CityResponse(
+    @SerializedName("accurate")
+    val accurate: String,
+    @SerializedName("cod")
+    val cod: String,
+    @SerializedName("count")
+    val count: Int,
+    @SerializedName("list")
+    val cityList: List<WeatherResponse>
+)
